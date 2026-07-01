2b0ad07053c7d9a3d7223553a23f6e978916478e
Attackeler <teocostin23@yahoo.com>
2026-07-01 22:07:38 +0300

    chore(db): set up versioned migrations workflow

---
+++ supabase/migrations/20260701183022_alter_dummy_table.sql.sql
@@ -0,0 +1,2 @@
+alter table if exists public.players
+add date_of_birth date not null;
---
+++ supabase/seed.sql
@@ -0,0 +1,4 @@
+insert into public.players (username, date_of_birth)
+values
+('Cristiana', '2004-08-21'),
+('Teo','2001-12-17')
