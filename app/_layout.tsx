f7e9e6008dbbac61125dedea92e7b25993797c75
Attackeler <teocostin23@yahoo.com>
2026-07-06 07:53:30 +0300

    chore: import fonts in _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from 'expo-router';
+import { useFonts } from '@expo-google-fonts/inter/useFonts';
+import { Inter_400Regular } from '@expo-google-fonts/inter/400Regular';
+import { Inter_600SemiBold } from '@expo-google-fonts/inter/600SemiBold';
+import { Inter_700Bold } from '@expo-google-fonts/inter/700Bold';
 
+export function useInterFonts() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_600SemiBold,
+        Inter_700Bold
+    });
+    return fontsLoaded;
+}
 export default function RootLayout() {
-    return <Stack />;
+    const fontsLoaded = useInterFonts();
+    if (!fontsLoaded) {
+        return null;
+    } else {
+        return <Stack />;
+    }
 }
