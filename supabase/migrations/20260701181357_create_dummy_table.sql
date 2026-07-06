ece04e02ac13aefcb36ca6bf7676c965a88debbf
Attackeler <teocostin23@yahoo.com>
2026-07-06 05:58:13 +0300

    refactor: remove dummy migrations and seed

--- supabase/migrations/20260701181357_create_dummy_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE table if not exists players (
-    id bigint generated always as identity primary key,
-    username text not null,
-    score int,
-    friends_number int
-)
--- supabase/seed.sql
+++
@@ -1,4 +0,0 @@
-insert into public.players (username, date_of_birth)
-values
-('Cristiana', '2004-08-21'),
-('Teo','2001-12-17')
